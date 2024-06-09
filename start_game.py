fd44925bb20d4135978c11f1b2c21bd46b5f8993
楊凱捷 <kjyang@KJyang.local>
2024-06-10 00:15:23 +0800

    randomforest

--- start_game.py
+++ start_game.py
@@ -5,7 +5,7 @@ from agents.random_player import setup_ai as random_ai
 from agents.console_player import setup_ai as console_ai
 from agents.my_player import setup_ai as my_ai
 from agents.classify_player import setup_ai as classify_ai
-from baseline0 import setup_ai as baseline0_ai
+from baseline1 import setup_ai as baseline1_ai
 
 '''
 from baseline0 import setup_ai as baseline0_ai
@@ -20,7 +20,7 @@ from baseline7 import setup_ai as baseline7_ai
 
 config = setup_config(max_round=20, initial_stack=1000, small_blind_amount=5)
 #config.register_player(name="p1", algorithm=baseline7_ai())
-config.register_player(name="p2", algorithm=baseline0_ai())
+config.register_player(name="p2", algorithm=baseline1_ai())
 
 ## Play in interactive mode if uncomment
 config.register_player(name="me", algorithm=classify_ai())
