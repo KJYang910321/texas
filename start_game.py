9d6221bfd5f35ca079f76f84cfcb295e261d3eff
楊凱捷 <kjyang@KJyang.local>
2024-06-09 16:59:57 +0800

    my

--- start_game.py
+++ start_game.py
@@ -6,10 +6,15 @@ from agents.console_player import setup_ai as console_ai
 from agents.my_player import setup_ai as my_ai
 from agents.classify_player import setup_ai as classify_ai
 from baseline0 import setup_ai as baseline0_ai
+from baseline1 import setup_ai as baseline1_ai
+from baseline2 import setup_ai as baseline2_ai
+from baseline3 import setup_ai as baseline3_ai
+from baseline4 import setup_ai as baseline4_ai
+from baseline5 import setup_ai as baseline5_ai
 
 config = setup_config(max_round=20, initial_stack=1000, small_blind_amount=5)
-config.register_player(name="p1", algorithm=baseline0_ai())
-config.register_player(name="p2", algorithm=random_ai())
+config.register_player(name="p1", algorithm=baseline1_ai())
+#config.register_player(name="p2", algorithm=random_ai())
 
 ## Play in interactive mode if uncomment
 config.register_player(name="me", algorithm=my_ai())
