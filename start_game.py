ebb9a89a8621318044cb099315a7a6d1245725b4
楊凱捷 <kjyang@KJyang.local>
2024-06-09 16:41:19 +0800

    my

--- start_game.py
+++ start_game.py
@@ -5,15 +5,14 @@ from agents.random_player import setup_ai as random_ai
 from agents.console_player import setup_ai as console_ai
 from agents.my_player import setup_ai as my_ai
 from agents.classify_player import setup_ai as classify_ai
-
-#from baseline0 import setup_ai as baseline0_ai
+from baseline0 import setup_ai as baseline0_ai
 
 config = setup_config(max_round=20, initial_stack=1000, small_blind_amount=5)
-#config.register_player(name="p1", algorithm=baseline0_ai())
+config.register_player(name="p1", algorithm=baseline0_ai())
 config.register_player(name="p2", algorithm=random_ai())
 
 ## Play in interactive mode if uncomment
-config.register_player(name="me", algorithm=classify_ai())
+config.register_player(name="me", algorithm=my_ai())
 game_result = start_poker(config, verbose=1)
 
 print(json.dumps(game_result, indent=4))
