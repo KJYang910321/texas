f390645332d598c893fa3d8aef720aa8f1b18df0
楊凱捷 <kjyang@KJyang.local>
2024-06-11 23:55:34 +0800

    v2

--- start_game.py
+++ start_game.py
@@ -7,6 +7,9 @@ from agents.my_player import setup_ai as my_ai
 from agents.classify_player import setup_ai as classify_ai
 from agents.my_player_v2 import setup_ai as my_player_v2_ai
 from baseline1 import setup_ai as baseline1_ai
+from baseline2 import setup_ai as baseline2_ai
+from baseline3 import setup_ai as baseline3_ai
+from baseline4 import setup_ai as baseline4_ai
 
 '''
 from baseline0 import setup_ai as baseline0_ai
@@ -20,7 +23,7 @@ from baseline7 import setup_ai as baseline7_ai
 '''
 
 config = setup_config(max_round=20, initial_stack=1000, small_blind_amount=5)
-config.register_player(name="baseline_1", algorithm=baseline1_ai())
+config.register_player(name="baseline_1", algorithm=baseline2_ai())
 #config.register_player(name="p2", algorithm=console_ai())
 #config.register_player(name="testing_my", algorithm=my_ai())
 #config.register_player(name="random", algorithm=random_ai())
